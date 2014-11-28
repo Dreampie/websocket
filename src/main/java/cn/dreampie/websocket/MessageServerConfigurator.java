aad6a64f3ba53d26732847183ed87bf7b9e0d5f2
Dreampie <wangrenhui1990@gmail.com>
2014-11-28 18:56:24 +0800

    Initial commit

---
+++ src/main/java/cn/dreampie/websocket/MessageServerConfigurator.java
@@ -0,0 +1,20 @@
+package cn.dreampie.websocket;
+
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
+
+/**
+ * Created by ice on 14-9-17.
+ */
+public class MessageServerConfigurator extends ServerEndpointConfig.Configurator {
+
+  @Override
+  public void modifyHandshake(ServerEndpointConfig conf,
+                              HandshakeRequest req,
+                              HandshakeResponse resp) {
+
+    conf.getUserProperties().put("handshakereq", req);
+  }
+
+}
